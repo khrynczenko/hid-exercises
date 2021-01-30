4631203b496ace82bdf81bf33cec2bd4c08be8d6
Krzysztof Hrynczenko <jeniopy@gmail.com>
2021-01-30 10:39:05 +0100

    finish chapter 3

--- chapter3/stockquotes/app/QuoteData.hs
+++ chapter3/stockquotes/app/QuoteData.hs
@@ -1,8 +1,45 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+
 module QuoteData where
 
-data QuoteData = QuoteData
+import GHC.Generics (Generic)
 
-data QuoteDataCollection = QuoteDataCollection
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+import Data.ByteString.Char8 (unpack)
+import Data.Csv (decodeByName, FromNamedRecord, FromField (..))
+import Data.Time (Day, parseTimeM, defaultTimeLocale)
 
-readQuotes :: FilePath -> IO QuoteDataCollection
-readQuotes filePath = undefined
+instance FromField Day where
+    parseField field =
+        parseTimeM False defaultTimeLocale "%Y/%m/%d" $ unpack field
+
+data QuoteData = QuoteData {
+    day :: Day,
+    volume :: Double,
+    open :: Double,
+    close :: Double,
+    high :: Double,
+    low :: Double
+} deriving (Generic, FromNamedRecord, Show)
+
+data QField = Open | Close | High | Low | Volume
+    deriving (Eq, Ord, Show, Enum, Bounded)
+
+
+field2fun :: QField -> QuoteData -> Double
+field2fun Open quoteData = open quoteData
+field2fun Close quoteData  = close quoteData
+field2fun High quoteData = high quoteData
+field2fun Low quoteData  = low quoteData
+field2fun Volume quoteData = volume quoteData
+
+readQuotes :: FilePath -> IO [QuoteData]
+readQuotes filePath = do
+    csvData <- BL.readFile filePath
+    case decodeByName csvData of
+        Left err -> do
+            putStrLn err
+            pure []
+        Right (_h, row) -> pure $ foldl (flip (:)) [] row
--- chapter3/stockquotes/app/StatReport.hs
+++ chapter3/stockquotes/app/StatReport.hs
@@ -1,11 +1,80 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module StatReport where
+
+import Fmt
+import Colonnade
+import Data.Ord (comparing)
+import Data.Foldable (minimumBy, maximumBy)
+import Data.Time (diffDays)
 
 import QuoteData
 
 data StatInfo = StatInfo
 
-statInfo :: QuoteDataCollection -> StatInfo
-statInfo quotes = undefined
+decimalPlacesFloating = 2
 
-textReport :: StatInfo -> String
-textReport info = undefined
+data StatValue = StatValue { decimalPlaces :: Int, value :: Double }
+
+data StatEntry = StatEntry { 
+    qfield :: QField,
+    meanVal :: StatValue,
+    minVal :: StatValue,
+    maxVal :: StatValue,
+    daysBetweenMinMax :: Int
+}
+
+instance Buildable StatValue where
+    build stat = fixedF (decimalPlaces stat) (value stat)
+
+instance Buildable StatEntry where
+    build StatEntry {..} =
+        "Stats for " +|| qfield ||+ ": "
+        +|meanVal|+" (mean), "
+        +|minVal|+" (min), "
+        +|maxVal|+" (max), "
+        +|daysBetweenMinMax|+" (days)"
+
+showPrice :: Double -> Builder
+showPrice v = fixedF decimalPlacesFloating v
+
+mean :: (Fractional a, Foldable t) => t a -> a
+mean xs = sum xs / fromIntegral (length xs)
+
+computeMinMaxDays :: (Ord a, Foldable t) => (QuoteData -> a) -> t QuoteData -> (a, a, Int)
+computeMinMaxDays get quotes = (get minQ, get maxQ, days)
+  where
+    cmp = comparing get
+    minQ = minimumBy cmp quotes
+    maxQ = maximumBy cmp quotes
+    days = fromIntegral $ abs $ diffDays (day minQ) (day maxQ)
+
+
+statInfo :: (Functor t, Foldable t) => t QuoteData -> [StatEntry]
+statInfo quotes = fmap qFieldStatInfo [minBound .. maxBound]
+  where
+    decimalPlacesByQField Volume = 0
+    decimalPlacesByQField _ = decimalPlacesFloating
+
+    qFieldStatInfo qfield =
+        let
+          get = field2fun qfield
+          (mn, mx, daysBetweenMinMax) = computeMinMaxDays get quotes
+          decPlaces = decimalPlacesByQField qfield
+          meanVal = StatValue decimalPlacesFloating (mean $ fmap get quotes)
+          minVal = StatValue decPlaces mn
+          maxVal = StatValue decPlaces mx
+        in StatEntry {..}
+
+
+textReport :: [StatEntry] -> String
+textReport = ascii colStats
+  where
+    colStats = mconcat
+        [ headed "Quote Field" (show . qfield)
+        , headed "Mean" (pretty . meanVal)
+        , headed "Min" (pretty . minVal)
+        , headed "Max" (pretty . maxVal)
+        , headed "Days between Min/Max" (pretty . daysBetweenMinMax)
+        ]
