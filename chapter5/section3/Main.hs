dc4d69eab23d0b0a7fd31013a09194de1e26e617
Krzysztof Hrynczenko <jeniopy@gmail.com>
2021-02-02 17:54:12 +0100

    finish chapter 5 section 3 subsection 1

---
+++ chapter5/section3/Main.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import Data.IORef
+import Text.Read
+
+mainIORef :: IO Int
+mainIORef = do
+    s <- newIORef 0
+    go s
+  where
+    go acc = readNumber >>= processNumber acc
+    
+    readNumber :: IO (Maybe Int)
+    readNumber = do
+        putStr "Put integer number (not a number to finish): "
+        readMaybe <$> getLine
+
+    processNumber acc Nothing = readIORef acc
+    processNumber acc (Just n) = fmap (+n) (readIORef acc) >>= writeIORef acc >> go acc
+    
+
+main :: IO ()
+main = do
+    s <- mainIORef
+    putStrLn "You sum is: "
+    print s
