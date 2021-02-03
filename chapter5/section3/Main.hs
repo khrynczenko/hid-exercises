02d9f7746e134e19baffb16de49d5ce6e0511b73
Krzysztof Hrynczenko <jeniopy@gmail.com>
2021-02-03 19:20:55 +0100

    finish chapter 5 section 3 subsection 2

--- chapter5/section3/Main.hs
+++ chapter5/section3/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
+import Control.Monad.ST
+import Control.Monad
 import Data.IORef
+import Data.STRef
 import Text.Read
 
 mainIORef :: IO Int
@@ -18,9 +21,18 @@ mainIORef = do
     processNumber acc Nothing = readIORef acc
     processNumber acc (Just n) = modifyIORef' acc (+n) >> go acc
     
+--main :: IO ()
+--main = do
+    --s <- mainIORef
+    --putStrLn "You sum is: "
+    --print s
+
+countZeroST :: [Int] -> ST s Int
+countZeroST xs = do
+    ref <- newSTRef 0
+    forM_ xs (\x -> if x == 0 then modifySTRef ref (+1) else pure ())
+    readSTRef ref
 
 main :: IO ()
 main = do
-    s <- mainIORef
-    putStrLn "You sum is: "
-    print s
+    print $ runST $ countZeroST [0,1,0,1,0]
